2386af2cfd2220f329004ea6db846a623e078ea2
Paul <ahmadzufukli@gmail.com>
2023-10-08 21:56:28 +0800

    add some lines

--- php/login-acc.php
+++ php/login-acc.php
@@ -6,6 +6,7 @@ if (isset($_POST["login"])) {
 
     echo "trying terminal in pushing update";
     echo "trying terminal in pushing update";
+    echo "trying terminal in pushing update";
 
     if (empty($password)) {
         echo "<script>alert('Please enter your password')</script>";
